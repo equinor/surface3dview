fabb6732039f04a0b0f2303af62849afd89e1af2
hauk88 <hakon.osterbo@bluware.com>
2022-02-15 11:02:13 +0100

    First implementation of marker. Needs refinements

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,5 +1,5 @@
-import { Suspense, useState, useMemo } from 'react'
-import { Object3D, Vector3, TextureLoader } from 'three'
+import { Suspense, useState, useMemo, useEffect } from 'react'
+import { Object3D, Vector3, TextureLoader, DataTexture } from 'three'
 import { Canvas, useLoader } from '@react-three/fiber'
 import { Stats, OrbitControls, PerspectiveCamera } from '@react-three/drei'
 import { Grid, Surface } from 'surface-3d-viewer'
@@ -10,21 +10,64 @@ import GpuCheck from './GpuCheck'
 Object3D.DefaultUp.set(0, 0, 1)
 
 // eslint-disable-next-line  @typescript-eslint/no-explicit-any
-const SurfaceContainer = ({ scale, ticks, domains }: any) => {
-    const [map, depth] = useLoader(TextureLoader, ['./sinc.png', './sinc_gray.png'])
+const SurfaceContainer = ({ scale, ticks, domains, marker }: any) => {
+
+    // const [map, depth] = useLoader(TextureLoader, ['./sinc.png', './sinc_gray.png'])
+    const [map, setMap] = useState(new DataTexture());
+    const [depth, setDepth] = useState(new DataTexture());
+
+    useEffect(()=>{
+        readImages();
+    }, [])
+
+    const readImages = async () => {
+        await imageDataFromSource('./sinc.png').then(v =>
+            {
+                const mapn = new DataTexture(); 
+                mapn.image = v;
+                mapn.flipY = true;
+                setMap(mapn);
+            }
+        )
+        await imageDataFromSource('./sinc_gray.png').then(v =>
+                { 
+                    const depthn = new DataTexture();
+                    depthn.image = v;
+                    setDepth(depthn);
+                }
+            )
+    }
+
+
 
     return (
         <Suspense fallback={null}>
             <Grid scale={scale} domains={domains} ticks={ticks} />
-            <Surface map={map} depth={depth} scale={scale} />
+            <Surface map={map} depth={depth} scale={scale} marker={marker} />
         </Suspense>
     )
 }
+
+async function imageDataFromSource (source: string) {
+    const image = Object.assign(new Image(), { src: source });
+    await new Promise<void>(resolve => image.addEventListener('load', () => resolve()));
+    const context = Object.assign(document.createElement('canvas'), {
+       width: image.width,
+       height: image.height
+    }).getContext('2d');
+    if(context){
+        context.imageSmoothingEnabled = false;
+        context.drawImage(image, 0, 0);
+        return context.getImageData(0, 0, image.width, image.height);
+    }
+    return new ImageData(1,1);
+ }
 
 const App = () => {
     const [scale, set] = useState(new Vector3(1, 1, 0.2))
     const [x, setX] = useState(100)
     const [ticks, setT] = useState(15)
+    const [marker, setMarker] = useState(false)
 
     const domains = useMemo(
         () => ({
@@ -37,18 +80,20 @@ const App = () => {
 
     return (
         <div className="canvas">
-            <Control z={scale.z} setZ={(z: number) => set((v) => new Vector3(v.x, v.y, z))} x={x} setX={setX} t={ticks} setT={setT} />
-            <Canvas frameloop="demand" linear flat>
+            <Control z={scale.z} setZ={(z: number) => set((v) => new Vector3(v.x, v.y, z))} x={x} setX={setX} t={ticks} setT={setT} c={marker} setC={setMarker} />
+            
+            <Canvas frameloop="demand" linear flat >
                 <ambientLight intensity={1.0} />
                 <pointLight intensity={0.1} position={[0.5, 0.5, 2]} />
                 <pointLight intensity={0.1} position={[-0.5, -0.5, 2]} />
                 <Suspense fallback={null}>
-                    <SurfaceContainer scale={scale} ticks={ticks} domains={domains} />
+                    <SurfaceContainer scale={scale} ticks={ticks} domains={domains} marker={marker}/>
                 </Suspense>
                 <PerspectiveCamera position={[-0.5, -1.0, 1.5]} near={0.01} far={1000} makeDefault />
                 <OrbitControls target={[0.5, 0.5, 0]} />
                 {process.env.NODE_ENV === 'development' && <Stats />}
             </Canvas>
+            
             <GpuCheck />
         </div>
     )
--- src/Surface.tsx
+++ src/Surface.tsx
@@ -1,9 +1,14 @@
-import { DoubleSide, DataTexture, Texture, Vector3, Shader } from 'three'
+import { ThreeEvent, useFrame, useThree } from '@react-three/fiber';
+import { Suspense, useEffect, useRef, useState } from 'react';
+import { DoubleSide, DataTexture, Texture, Vector3, Shader, BufferGeometry, PlaneBufferGeometry } from 'three'
+import { Text } from './Text'
 
 interface IProps {
-    map: DataTexture | Texture
-    depth: DataTexture | Texture
+    map: DataTexture
+    depth: DataTexture
     scale: Vector3
+
+    marker?: boolean
 
     metalness?: number
     roughness?: number
@@ -17,25 +22,165 @@ interface IProps {
  * Depth ~ bump scale texture (gray scale)
  * Scale ~ vector of how to scale the surface
  */
-const Surface = ({ map, depth, scale, ...props }: IProps) => {
-    return (
-        <mesh position={[0.5 * scale.x, 0.5 * scale.y, 0]} scale={scale}>
-            <planeBufferGeometry attach="geometry" args={[1, 1, depth.image.width + 1, depth.image.height + 1]} />
+const Surface = ({ map, depth, scale,...props }: IProps) => {
+    const [markerPos, setMarkerPos] = useState(new Vector3(0, 0, 0))
+    const [textPos, settextPos] = useState(markerPos)
+    const [textValue, setTextValue] = useState(0);
+    const [geom, setGeom] = useState(new PlaneBufferGeometry())
+    const marker = useRef(new BufferGeometry().setFromPoints([markerPos, markerPos]))
 
-            <meshStandardMaterial
-                map={map}
-                metalness={0.1}
-                roughness={0.6}
-                side={DoubleSide}
-                alphaToCoverage
-                onBeforeCompile={(shader) => setUpShader(shader, depth)}
-                {...props}
-            />
-        </mesh>
+    // visible would not work with setting undefined hence explisitly use true/false
+    const renderMarker = props.marker ? true : false;
+    
+    const handleMouseHover = (e: ThreeEvent<PointerEvent>) => {
+        e.stopPropagation()
+        setMarkerPos(e.point)
+    };
+
+    const onMouseHover = renderMarker ? handleMouseHover : undefined;
+
+    useEffect(
+        () => {
+            const pos = marker.current.getAttribute("position")
+            const pa = pos.array as number[]
+
+
+
+            const z = markerPos.z + Math.max((scale.z - markerPos.z), 0.1) ;
+            settextPos(new Vector3(markerPos.x, markerPos.y, z))
+
+            pa[0] = markerPos.x
+            pa[3] = markerPos.x
+
+            pa[1] = markerPos.y
+            pa[4] = markerPos.y
+
+            pa[2] = markerPos.z
+            pa[5] = z
+            pos.needsUpdate = true;
+
+            
+            setTextValue(Math.round(markerPos.z * 1000) / 1000);
+
+        }, [markerPos, scale]
+    )
+
+    const r = depth.image.width / depth.image.height;
+    const t = 2000;
+    const y = Math.floor(Math.sqrt(t / r))
+    const x = Math.floor(r * y);
+
+    // const x = depth.image.width
+    // const y = depth.image.height
+
+    useEffect(
+        () => {
+            if (depth.image.data == null) {
+                return;
+            }
+            console.log([x, y])
+
+            const geomn = new PlaneBufferGeometry(1, 1, x, y)
+            const pos = geomn.getAttribute("position");
+            const pa = pos.array as number[];
+            const w = x + 1;
+            const h = y + 1;
+            for (let j = 0; j < h; j++) {
+                for (let i = 0; i < w; i++) {
+                    const idx = j * w + i;
+                    const imgI = Math.floor((depth.image.width - 1) * i / (w - 1))
+                    const imgJ = Math.floor((depth.image.height - 1) * j / (h - 1))
+                    const idxmap = imgJ * depth.image.width + imgI
+                    let d = depth.image.data[4 * idxmap] / 255
+                    if (isNaN(d))
+                        d = 0;
+                    pa[3 * idx + 2] = d;
+                }
+            }
+
+            setGeom(geomn);
+        }, [depth]
+    )
+
+
+    return (
+        <Suspense fallback={null}>
+            <mesh position={[0.5 * scale.x, 0.5 * scale.y, 0]} scale={scale} onPointerMove={onMouseHover} geometry={geom}>
+                <meshStandardMaterial
+                    map={map}
+                    metalness={0.1}
+                    roughness={0.6}
+                    side={DoubleSide}
+                    alphaToCoverage
+                    {...props}
+                />
+
+            </mesh>
+
+            <line_ key={marker.current.uuid} geometry={marker.current} visible={renderMarker}>
+                <meshBasicMaterial
+                    attach="material"
+                    color={'black'}
+                    polygonOffset
+                    polygonOffsetFactor={GRIDOffsetFactor}
+                    polygonOffsetUnits={GRIDOffsetUnits}
+                />
+            </line_>
+
+            <Tick x={textValue} anchorX={'right'} position={textPos} visible={renderMarker}/>
+
+
+        </Suspense>
     )
 }
 
+interface ITick {
+    x: number
+    anchorX: 'right' | 'left'
+    position: Vector3
+
+    visible?:boolean
+}
+
+const Tick = ({ x, anchorX, position, ...props } : ITick) => {
+    const { camera } = useThree()
+    const ref = useRef<Text>()
+
+    // Change text rotation
+    useFrame(() => {
+        // @ts-ignore
+        if (ref.current) ref.current.quaternion.copy(camera.quaternion)
+    })
+
+    let t = x.toString()
+    if (anchorX === 'right') t += '-'
+    else t = '-' + t
+
+    return (
+        <Text
+            ref={ref}
+            position={position}
+            anchorX={anchorX}
+            anchorY="middle"
+            outlineColor={'white'}
+            outlineWidth={0.0025}
+            fontSize={0.025}
+            strokeWidth={1}
+            strokeColor={'black'}
+            fillOpacity={0}
+            {...props}
+        >
+            {t}
+        </Text>
+    )
+}
+
+
 export default Surface
+
+
+const GRIDOffsetFactor = 10
+const GRIDOffsetUnits = 1
 
 function setUpShader(shader: Shader, textureDepth: DataTexture | Texture) {
     // uniforms
